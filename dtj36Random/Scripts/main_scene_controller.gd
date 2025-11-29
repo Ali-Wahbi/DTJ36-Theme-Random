846c9ae0a5bc1133d523a3d6be01d660f5a42d75
Ali-Wahbi <aliwahbi436@gmail.com>
2025-11-29 22:39:01 +0400

    title added after second challenge

--- dtj36Random/Scripts/main_scene_controller.gd
+++ dtj36Random/Scripts/main_scene_controller.gd
@@ -1,43 +1,58 @@
 extends Node2D
 
-@export var challgeOne: Node2D
-@export var challgeTwo: Node2D
+@export var challengeOne: Node2D
+@export var challengeTwo: Node2D
+@export var title: Node2D
 
 
 @export_group("BG Colors")
 @export var bg: ColorRect
-@export var challgeOneBG: Color
-@export var challgeTwoBG: Color
-@export var challgeThreeBG: Color
-@export var challgeFourBG: Color
+@export var challengeOneBG: Color
+@export var challengeTwoBG: Color
+@export var titleBG: Color
+@export var challengeThreeBG: Color
+@export var challengeFourBG: Color
+
+
+@export_group("Challenges Delays")
+@export var challengeOneDelay: float
+@export var challengeTwoDelay: float
+@export var titleDelay: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hideAllChallenges()
 	setupSignals()
-
+	# startTitle()
 	startChallengeOne()
 
 
 func hideAllChallenges():
-	challgeOne.visible = false
-	challgeTwo.visible = false
+	challengeOne.visible = false
+	challengeTwo.visible = false
+	title.visible = false
 
 
 func startChallengeOne():
 	print("Starting challenge one")
-	challgeOne.visible = true
-	tweenBG(challgeOneBG)
-	await _create_timer(1)
-	challgeOne.showAllObjects()
+	challengeOne.visible = true
+	tweenBG(challengeOneBG)
+	await _create_timer(challengeOneDelay)
+	challengeOne.showAllObjects()
 
 func startChallengeTwo():
 	print("Starting challenge two")
-	challgeTwo.visible = true
-	tweenBG(challgeTwoBG)
-	await _create_timer(1)
-	challgeTwo.showAllObjects()
+	challengeTwo.visible = true
+	tweenBG(challengeTwoBG)
+	await _create_timer(challengeTwoDelay)
+	challengeTwo.showAllObjects()
 
+func startTitle():
+	print("Starting title")
+	title.visible = true
+	tweenBG(titleBG)
+	await _create_timer(titleDelay)
+	title.showAllObjects()
 
 func _create_timer(time: float = 0) -> Signal:
 	return get_tree().create_timer(time).timeout
@@ -47,8 +62,10 @@ func _process(_delta: float) -> void:
 	pass
 
 func setupSignals():
-	challgeOne.challengeIsDone.connect(onChallengeOneDone)
-	challgeTwo.challengeIsDone.connect(onChallengeTwoDone)
+	challengeOne.challengeIsDone.connect(onChallengeOneDone)
+	challengeTwo.challengeIsDone.connect(onChallengeTwoDone)
+	title.challengeIsDone.connect(onTitleDone)
+
 
 func tweenBG(color: Color, duration: float = 3.5):
 	var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
@@ -56,15 +73,22 @@ func tweenBG(color: Color, duration: float = 3.5):
 
 func onChallengeOneDone():
 	print("Challenge one done")
-	await _create_timer(2)
-	challgeOne.hideAllObjects(true)
-	await challgeOne.allObjectsHidden
+	await _create_timer(1)
+	challengeOne.hideAllObjects(true)
+	await challengeOne.allObjectsHidden
 	startChallengeTwo()
 
 
 func onChallengeTwoDone():
 	print("Challenge two done")
 	await _create_timer(1)
-	challgeTwo.hideAllObjects(true)
-	await challgeTwo.allObjectsHidden
-	# startChallengeTwo()
+	challengeTwo.hideAllObjects(true)
+	await challengeTwo.allObjectsHidden
+	startTitle()
+
+func onTitleDone():
+	print("Title done")
+	await _create_timer(1)
+	title.hideAllObjects()
+	await title.allObjectsHidden
+	# startChallengeOne()
---
+++ dtj36Random/Scripts/title.gd
@@ -0,0 +1,62 @@
+extends Node2D
+
+signal challengeIsDone
+signal allObjectsHidden
+
+@export var firstLabel: Label
+@export var secondLabel: Label
+@export var thirdLabel: Label
+@export var foarthLabel: Label
+
+@export var firstDelay: float = 0.5
+@export var secondDelay: float = 0.5
+@export var thirdDelay: float = 0.5
+@export var foarthDelay: float = 0.5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	firstLabel.visible = false
+	secondLabel.visible = false
+	thirdLabel.visible = false
+	foarthLabel.visible = false
+
+
+func _create_timer(time: float = 0) -> Signal:
+	return get_tree().create_timer(time).timeout
+
+func showAllObjects(duration: float = 0.5):
+	await _create_timer(firstDelay)
+	firstLabel.visible = true
+	await tweenFade(firstLabel, 0, 1, duration)
+
+	await _create_timer(secondDelay)
+	secondLabel.visible = true
+	await tweenFade(secondLabel, 0, 1, duration)
+
+	await _create_timer(thirdDelay)
+	thirdLabel.visible = true
+	await tweenFade(thirdLabel, 0, 1, duration)
+
+	await _create_timer(foarthDelay)
+	foarthLabel.visible = true
+	await tweenFade(foarthLabel, 0, 1, duration + 0.5)
+
+	challengeIsDone.emit()
+
+
+func hideAllObjects(duration: float = 0.5):
+	tweenFade(firstLabel, 1, 0, duration)
+	tweenFade(secondLabel, 1, 0, duration)
+	tweenFade(thirdLabel, 1, 0, duration)
+	tweenFade(foarthLabel, 1, 0, duration)
+
+	await _create_timer(duration * 4)
+	allObjectsHidden.emit()
+
+
+func tweenFade(object: Label, from: float, to: float, duration: float) -> Signal:
+	var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	object.modulate.a = from
+	tween.tween_property(object, "modulate:a", to, duration)
+	return tween.finished
